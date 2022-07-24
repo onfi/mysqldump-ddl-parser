8a83d542d268c339ff133f74cdc6b8301381df84
simodake <simodake@gmail.com>
2022-07-24 12:57:17 +0900

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ module.exports = {
   entry: `./dist/index.js`,
   output: {
     path: `${__dirname}/dist/`,
-    filename: `mysql-ddl-parser.min.js`,
-    library: 'MysqlDdlParser',
+    filename: `mysqldump-ddl-parser.min.js`,
+    library: 'MysqldumpDdlParser',
     libraryExport: 'default',
     libraryTarget: 'umd',
     globalObject: 'this',
